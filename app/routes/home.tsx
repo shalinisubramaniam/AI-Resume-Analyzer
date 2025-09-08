114b00ea1c342031b6e89da9a2950e9445b80df7
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-09-08 14:50:11 +0530

    implemented & styled navbar

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import Navbar from "../components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,6 +10,8 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    
     <section className="main-section">
       <div className="page-heading">
         <h1>Track Your Applications & Resume Ratings</h1>
