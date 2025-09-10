0ee8a66f65f7f8c1323d894c1fe2bdf84a4f709a
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-09-10 23:58:10 +0530

    started with ResumeCard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "../components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -17,6 +19,14 @@ export default function Home() {
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
     </section>
   </main>;
 }
